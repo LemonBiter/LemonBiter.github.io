2fa9a4f60162984ab846a4360099bb62738b9e83
Junhong Zhu <a1712762@student.adelaide.edu.au>
2020-03-15 11:57:07 +0800

    slider component add

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+module.exports = {
+    mode: 'production',
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, "dist"), // string
+        // 所有输出文件的目标路径
+        filename: 'bundle.js'
+    },
+    module: {
+        rules:[
+            {
+                test:/\.css$/,
+                use:['style-loader','css-loader']
+            },
+            {
+                test:/\.(jpg|jpeg|png|git)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[path][name].[ext]',
+                    // publicPath: '/images'
+                }
+
+            }
+        ]
+    }
+};
